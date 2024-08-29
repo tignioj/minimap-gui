e4e21b67c9a340006d1555d4bc9ace4902bae275
tignioj <859897994@qq.com>
2024-08-29 18:39:02 +0800

    更新鼠标坐标命名

--- utils/mouse.js
+++ utils/mouse.js
@@ -5,13 +5,19 @@ import { ref, onMounted, onUnmounted } from 'vue'
 // https://cn.vuejs.org/guide/reusability/composables
 export function useMouse() {
     // 被组合式函数封装和管理的状态
-    const mouseX = ref(0)
-    const mouseY = ref(0)
+    const mousePageX = ref(0)
+    const mousePageY = ref(0)
+
+    const mouseClientX= ref(0)
+    const mouseClientY= ref(0)
 
     // 组合式函数可以随时更改其状态。
     function update(event) {
-        mouseX.value = event.pageX
-        mouseY.value = event.pageY
+        mousePageX.value = event.pageX
+        mousePageY.value = event.pageY
+
+        mouseClientX.value = event.clientX
+        mouseClientY.value = event.clientY
     }
 
     // 一个组合式函数也可以挂靠在所属组件的生命周期上
@@ -20,5 +26,5 @@ export function useMouse() {
     onUnmounted(() => window.removeEventListener('mousemove', update))
 
     // 通过返回值暴露所管理的状态
-    return { mouseX, mouseY }
+    return { mousePageX, mousePageY ,mouseClientX, mouseClientY }
 }
