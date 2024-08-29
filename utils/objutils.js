c46237cebfeb2fa034ebeab2ef4b9160303dadd1
tignioj <859897994@qq.com>
2024-08-29 18:39:42 +0800

    使用JSON拷贝对象的工具

---
+++ utils/objutils.js
@@ -0,0 +1,12 @@
+
+// 按照惯例，组合式函数名以“use”开头
+// https://cn.vuejs.org/guide/reusability/composables
+/**
+ * 复制一个对象
+ * 注意：无法复制函数
+ * @param sourceObject
+ * @returns {any}
+ */
+export function copyObject(sourceObject) {
+    return JSON.parse(JSON.stringify(sourceObject))
+}
