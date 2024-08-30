70d0ee8ad08dbfa7c2c367f0868476c7cc7f4ba0
tignioj <859897994@qq.com>
2024-08-31 06:25:17 +0800

    keys

---
+++ src/keys.js
@@ -0,0 +1,6 @@
+export const injectKeyPointTypes= Symbol()
+export const injectKeyPointMoveModes= Symbol()
+export const injectKeyPointActions= Symbol()
+export const injectKeyCNTextMap = Symbol()
+export const injectKeyIconMap= Symbol()
+export const injectKeyRegions = Symbol()
