16034f9d16ed62bf8d635171ab24a3a295ef91b4
tignioj <859897994@qq.com>
2024-09-01 10:56:57 +0800

    路由更新

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,10 @@ main {
   </p>
   <nav>
     <RouterLink to="/">清单管理</RouterLink> |
+    <RouterLink to="/task/collect/edit">创建采集任务</RouterLink> |
+    <RouterLink to="/task/fight/edit">创建战斗任务</RouterLink> |
+    <RouterLink to="/task/gouliang/edit">创建狗粮任务</RouterLink> |
     <RouterLink to="/config/editor">配置</RouterLink> |
-    <RouterLink to="/task/base/new">创建基础路线</RouterLink> |
-    <RouterLink to="/task/collect/new">创建采集任务</RouterLink> |
-    <RouterLink to="/task/fight/new">创建战斗任务</RouterLink> |
-    <RouterLink to="/task/gouliang/new">创建狗粮任务</RouterLink>
   </nav>
   <main>
     <RouterView />
