1245460d5ffd15b874bf1aa7a6abea92ce33add6
tignioj <859897994@qq.com>
2024-09-04 21:27:36 +0800

    部分页面持久化

--- src/App.vue
+++ src/App.vue
@@ -15,24 +15,27 @@ const route = useRoute()
 const activateClass = (link) => {
   route.fullPath.includes(link)
 }
+const keepAlive = ['ConfigEditorPage', 'ScriptManagerPage']
 </script>
 <template>
   <p>
     <strong>Current route path:</strong> {{ $route.fullPath }}
   </p>
-  <nav>
-    <a @click="router.go(-1)" style="cursor: pointer">返回</a> |
-    <RouterLink :class="{'active-link': route.fullPath === '/'}" to="/">清单管理</RouterLink> |
-    <RouterLink :class="{'active-link': route.fullPath.includes('/task/collect/edit')}" to="/task/collect/edit">创建采集任务</RouterLink> |
-    <RouterLink :class="{'active-link': route.fullPath.includes('/task/fight/edit')}"  to="/task/fight/edit">创建战斗任务</RouterLink> |
-    <RouterLink :class="{'active-link': route.fullPath.includes('/task/gouliang/edit')}"  to="/task/gouliang/edit">创建狗粮任务</RouterLink> |
-    <RouterLink :class="{'active-link': route.fullPath === '/config/editor'}"  to="/config/editor">配置</RouterLink> |
-  </nav>
-  <main>
-    <router-view v-slot="{ Component }">
-      <keep-alive>
-        <component :is="Component" />
-      </keep-alive>
-    </router-view>
-  </main>
+    <nav>
+      <a @click="router.go(-1)" style="cursor: pointer">返回</a> |
+      <RouterLink :class="{'active-link': route.fullPath === '/'}" to="/">清单管理</RouterLink> |
+      <RouterLink :class="{'active-link': route.fullPath.includes('/task/collect/edit')}" to="/task/collect/edit">创建采集任务</RouterLink> |
+      <RouterLink :class="{'active-link': route.fullPath.includes('/task/fight/edit')}"  to="/task/fight/edit">创建战斗任务</RouterLink> |
+      <RouterLink :class="{'active-link': route.fullPath.includes('/task/gouliang/edit')}"  to="/task/gouliang/edit">创建狗粮任务</RouterLink> |
+      <RouterLink :class="{'active-link': route.fullPath === '/config/editor'}"  to="/config/editor">配置</RouterLink> |
+      <RouterLink :class="{'active-link': route.fullPath === '/logs'}"  to="/logs">日志</RouterLink> |
+    </nav>
+    <main>
+      <!--      <keep-alive :include="keepAlive">-->
+      <router-view v-slot="{ Component }">
+        <keep-alive :include="keepAlive">
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
+    </main>
 </template>
