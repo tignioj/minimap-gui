0a2bc8ec009f6ef55672f41f415b58342018eb78
tignioj <859897994@qq.com>
2024-09-10 15:13:51 +0800

    日志（没做）

---
+++ src/page/LogPage.vue
@@ -0,0 +1,22 @@
+<script setup>
+import {store} from "@/store.js";
+
+store.appendLogs("你好")
+
+</script>
+
+<template>
+  <textarea rows="60" readonly class="log">{{store.logs}}</textarea>
+</template>
+
+<style scoped>
+
+.log {
+  width: 100%;
+}
+
+.ace_editor .custom-color-red {
+  color: red;
+}
+
+</style>
