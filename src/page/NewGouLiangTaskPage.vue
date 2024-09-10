608252d85c179e17ab274b2a0bb0305bebed75b2
tignioj <859897994@qq.com>
2024-09-10 15:15:35 +0800

    添加纳塔选项

--- src/page/NewGouLiangTaskPage.vue
+++ src/page/NewGouLiangTaskPage.vue
@@ -42,7 +42,7 @@ const cnTextMap = {
 const actions=["", "stop_flying"];
 const pointTypes=["path", "target"];
 const moveModes = ["normal", "fly", "jump", "swim", "up_down_grab_leaf"]
-const regions = ["蒙德", "璃月", "须弥", "稻妻", "枫丹"];
+const regions = ["蒙德", "璃月", "须弥", "稻妻", "枫丹", "纳塔"];
 const executor = 'GouLiangPathExecutor'
 </script>
 <template>
