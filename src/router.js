0b8d71da0cc8c9eed79a8fb4fcf15ff7298deb83
tignioj <859897994@qq.com>
2024-09-10 15:14:07 +0800

    路由更新

--- src/router.js
+++ src/router.js
@@ -7,12 +7,15 @@ import NewFightTaskPage from "@/page/NewFightTaskPage.vue";
 import NewGouLiangTaskPage from "@/page/NewGouLiangTaskPage.vue";
 import LogPage from "@/page/LogPage.vue";
 import FightTeamManagerPage from "@/page/FightTeamManagerPage.vue";
+import AboutPage from "@/page/AboutPage.vue";
+import DailyMissionPage from "@/page/DailyMissionPage.vue";
 
 const routes = [
     { path: '/', component: ScriptManagerPage, meta: { keepAlive: true }, // 需要缓存
         // redirect: '/task/collect/edit',
         // redirect: '/team',
     },
+    { path: '/daily', component: DailyMissionPage},
     { path: '/config/editor', component: ConfigEditorPage , meta: { keepAlive: true } },
     { path: '/task/collect/edit', component: NewCollectTaskPage },
     { path: '/task/collect/edit/:fileName', component: NewCollectTaskPage },
@@ -23,8 +26,8 @@ const routes = [
     { path: '/task/gouliang/edit', component: NewGouLiangTaskPage},
     { path: '/task/gouliang/edit/:fileName', component: NewGouLiangTaskPage},
     { path: '/logs', component: LogPage},
-
     { path: '/team', component: FightTeamManagerPage},
+    { path: '/about', component: AboutPage},
 
 ]
 
