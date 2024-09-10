d33d4d5c3f8ca65fbe964d46c452542643b780f2
tignioj <859897994@qq.com>
2024-09-10 15:16:26 +0800

    更新路由

--- src/App.vue
+++ src/App.vue
@@ -27,11 +27,13 @@ const keepAlive = ['ConfigEditorPage', 'ScriptManagerPage']
       <RouterLink :class="{'active-link': route.fullPath.includes('/task/collect/edit')}" to="/task/collect/edit">创建采集任务</RouterLink> |
       <RouterLink :class="{'active-link': route.fullPath.includes('/task/fight/edit')}"  to="/task/fight/edit">创建战斗任务</RouterLink> |
       <RouterLink :class="{'active-link': route.fullPath.includes('/task/gouliang/edit')}"  to="/task/gouliang/edit">创建狗粮任务</RouterLink> |
+      <RouterLink :class="{'active-link': route.fullPath === '/daily'}" to="/daily">每日委托</RouterLink> |
+      <RouterLink :class="{'active-link': route.fullPath === '/team'}"  to="/team">战斗队伍管理</RouterLink> |
       <RouterLink :class="{'active-link': route.fullPath === '/config/editor'}"  to="/config/editor">配置</RouterLink> |
       <RouterLink :class="{'active-link': route.fullPath === '/logs'}"  to="/logs">日志</RouterLink> |
+      <RouterLink :class="{'active-link': route.fullPath === '/about'}"  to="/about">关于</RouterLink> |
     </nav>
     <main>
-      <!--      <keep-alive :include="keepAlive">-->
       <router-view v-slot="{ Component }">
         <keep-alive :include="keepAlive">
           <component :is="Component" />
