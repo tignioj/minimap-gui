674eb4031f6cb510cd174824ca8b9650898755b6
tignioj <859897994@qq.com>
2024-09-12 08:18:59 +0800

    地脉编辑

---
+++ src/page/NewLeyLineOutCropPage.vue
@@ -0,0 +1,66 @@
+<script setup>
+
+import NewTaskPage from "@/components/NewTaskPage.vue";
+import {
+  faArrowTrendUp, faBan, faBullseye,
+  faClover, faExpand, faGear, faKey,
+  faMapMarkerAlt,
+  faPlane,
+  faPlaneArrival, faQuestion,
+  faWalking,
+  faWater
+} from "@fortawesome/free-solid-svg-icons";
+import Manual from "@/components/task/Manual.vue";
+// https://fontawesome.com/search
+const iconMapping = {
+  'normal': faWalking,
+  'fly': faPlane,
+  'jump': faArrowTrendUp,
+  'swim': faWater,
+  'up_down_grab_leaf': faClover,
+  'stop_flying': faPlaneArrival,
+  'path': faMapMarkerAlt,
+  'target': faBullseye,
+  'nahida_collect': faExpand,
+  '': faBan,
+  undefined: faQuestion,
+  null: faQuestion,
+  'fight': faGear,
+  'reward': faKey
+};
+const cnTextMap = {
+  'normal': '普通',
+  'fly': '飞行',
+  'jump': '跳跃',
+  'swim': '游泳',
+  'up_down_grab_leaf': '四叶印',
+  'path':'路径',
+  'target': '目标',
+  'nahida_collect':'纳西妲采集',
+  '': '无',
+  'stop_flying': '下落攻击',
+  'fight': '战斗',
+  'reward': '领取奖励'
+}
+
+const actions=["", "stop_flying", "fight", "reward"];
+const pointTypes=["path", "target"];
+const moveModes = ["normal", "fly", "jump", "swim", "up_down_grab_leaf"]
+const regions = ["蒙德", "璃月", "须弥", "稻妻", "枫丹", "纳塔"];
+const executor = 'LeyLineOutcropPathExecutor'
+</script>
+<template>
+<NewTaskPage :executor="executor"
+             :move-modes="moveModes"
+             :point-types="pointTypes"
+             :actions="actions"
+             :cn-text-map="cnTextMap"
+             :icon-mapping="iconMapping"
+             :regions="regions"
+             />
+
+<!--  <Manual/>-->
+</template>
+<style scoped>
+
+</style>
