101ed2544097e8c30ffaefa7df02a8aa1551dc0c
tignioj <859897994@qq.com>
2024-09-12 09:09:13 +0800

    更新说明

--- src/page/DailyMissionPage.vue
+++ src/page/DailyMissionPage.vue
@@ -75,11 +75,12 @@ onMounted(()=> {
   <button @click="dailyMissionRun">执行每日委托</button>
   <button @click="dailyMissionStop">停止执行</button>
   <h3>使用手册</h3>
-  <p>1. 将委托地点调整到蒙德，等委托更新后才可以使用，仅能完成战斗委托。</p>
+  <p>1. 在游戏中将委托地点调整到蒙德，等第二天委托更新后才可以使用。注：仅能完成战斗委托。</p>
   <p>2. 前往<RouterLink to="/team">战斗队伍管理</RouterLink> 中设置或者添加您的默认队伍,怪物可能包含各种元素盾,确保您的队伍有足够的伤害完成委托。</p>
   <p>3. 建议队伍中配备风c以应对各种元素盾。 </p>
   <p>4. 根据战斗委托的特性: '按数量消灭怪物时，若第一次没打完全部怪物，第二次再去, 仅需要打剩下未死亡的怪物', 因此无需担心首次没做完，本程序会一直检测直到超时。</p>
   <p>5. 超时时间以及其他委托相关配置请在<RouterLink to="/config/editor">配置</RouterLink>中找到'委托配置'。 </p>
+  <p>6. 角色死亡会自动使用道具复活(但是不会吃鸡回血)，如需关掉请在配置的'路径执行器配置'中找到'enable_food_revive: 1', 将其设置为'enable_food_revive: 0'</p>
 
 </template>
 
