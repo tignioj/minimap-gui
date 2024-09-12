a9a9fca3e737fdad7753c5c6349f1b5209f9e2d2
tignioj <859897994@qq.com>
2024-09-12 08:19:06 +0800

    委托编辑

---
+++ src/page/NewDailyMissionPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import NewTaskPage from "@/components/NewTaskPage.vue";
+
+import Manual from "@/components/task/Manual.vue";
+// https://fontawesome.com/search
+
+const executor = 'DailyMissionPathExecutor'
+</script>
+<template>
+  <NewTaskPage :executor="executor" />
+    <Manual/>
+</template>
+<style scoped>
+
+</style>
