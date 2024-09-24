c2bed354a9c4390939c13478e822e55c6aa15f17
tignioj <859897994@qq.com>
2024-09-24 22:35:11 +0800

    生成Flask模板

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,27 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import flaskPlugin from './vite-plugin-flask'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    flaskPlugin(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    manifest: true, // 启用 manifest 选项
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
+    },
+  },
 })
