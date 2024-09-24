71299f56aa5d86343066ffbabee551337755105f
tignioj <859897994@qq.com>
2024-09-25 00:09:02 +0800

    flask模板插件仅在构建的时候生效，预览时候关闭

--- vite.config.js
+++ vite.config.js
@@ -5,24 +5,49 @@ import vue from '@vitejs/plugin-vue'
 import flaskPlugin from './vite-plugin-flask'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    flaskPlugin(),
-  ],
-  resolve: {
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     // flaskPlugin(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//   build: {
+//     manifest: true, // 启用 manifest 选项
+//     rollupOptions: {
+//       output: {
+//         entryFileNames: 'assets/[name]-[hash].js',
+//         chunkFileNames: 'assets/[name]-[hash].js',
+//         assetFileNames: 'assets/[name]-[hash].[ext]',
+//       },
+//     },
+//   },
+// })
+
+
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      vue(),
+      // 仅在 build 模式下启用 flaskPlugin
+      command === 'build' && flaskPlugin()
+    ],
+    resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  build: {
-    manifest: true, // 启用 manifest 选项
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/[name]-[hash].js',
+          chunkFileNames: 'assets/[name]-[hash].js',
+          assetFileNames: 'assets/[name]-[hash].[ext]',
+        },
       },
     },
-  },
-})
+  };
+});
